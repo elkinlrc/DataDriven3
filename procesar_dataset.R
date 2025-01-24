0f7b39a8b6d56fdf6416637b7306eb8b35e009c1
DarioChavez91 <dario_ch_a@hotmail.com>
2025-01-24 10:34:56 +0100

    Limpiado de datos, acomodado de fecha (falta formatear) Graficos completados en base a metodo y respuesta

--- procesar_dataset.R
+++ procesar_dataset.R
@@ -31,8 +31,9 @@ procesar_dataset <- function(file_path = "datos/epa-http.csv") {
   
   # Limpiar columnas
   dataset$Metodo <- str_replace_all(dataset$Metodo, '"', "")
-  dataset$Timestamp <- as.POSIXct(dataset$Timestamp, format = "[%d/%b/%Y:%H:%M:%S %z]", tz = "UTC")
-  
+  dataset$Protocolo <- str_replace_all(dataset$Protocolo, '"', "")
+  dataset$Timestamp <- str_replace_all(dataset$Timestamp, "\\[|\\]", "")
+  #dataset$Timestamp <- as.POSIXct(dataset$Timestamp, format = "[%d/:%H:%M:%S]", tz = "UTC")
   # Crear columna adicional para longitud de Endpoint
   dataset <- dataset %>% mutate(Longitud_Endpoint = str_length(Endpoint))
   
@@ -41,3 +42,37 @@ procesar_dataset <- function(file_path = "datos/epa-http.csv") {
 
 # Procesar y describir el dataset
 
+
+dataset <- procesar_dataset()
+summary(dataset)
+
+#Crear un grafico en base a respuestas del servidor
+# Resumir códigos de estado
+Respuesta_summary <- table(dataset$Respuesta_http)
+Respuesta_percent <- prop.table(Respuesta_summary) * 100
+# Darle color al pastel
+library(RColorBrewer)
+myPalette <- brewer.pal(5, "Set2") 
+
+# Crear gráfico de pastel
+pie_chart <- pie(
+  Respuesta_percent,
+  labels = paste0(names(Respuesta_percent), " (", round(Respuesta_percent, 1), "%)"),
+  main = "Distribución de Códigos Respuestas Http",border="white", col=myPalette
+)
+
+
+#Crear un grafico en base a los Metodos usados
+# Resumir códigos de estado
+Metodo_summary <- table(dataset$Metodo)
+Metodo_percent <- prop.table(Metodo_summary) * 100
+# Darle color al pastel
+library(RColorBrewer)
+myPalette <- brewer.pal(5, "Set3") 
+
+# Crear gráfico de pastel
+pie_chart <- pie(
+  Metodo_percent,
+  labels = paste0(names(Metodo_percent), " (", round(Metodo_percent, 1), "%)"),
+  main = "Distribución de Metodos",border="white", col=myPalette
+)
